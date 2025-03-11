211b9eb1d4dd8755cf037d2909330e6e82a6a346
database-moscow <ispoap@gmail.com>
2025-03-11 17:29:25 +0700

    Add files via upload

--- binary.h
+++ binary.h
@@ -1,9 +1,10 @@
 #include <string>
 #include <cmath>
 using namespace std;
-string binary(int kk, int s){
+string binary(double k, int s){
     string e, ee;
     string m;
+    int kk=int(k);
     if(kk<0){
         kk*=(-1);
         m='-';
@@ -37,24 +38,61 @@ string binary(int kk, int s){
         }
         e=ee+e;
     }
+    if(int(k)!=k){
+        e=e+'.';
+        double drch=k-int(k);
+        while(drch!=0){
+        drch*=s;
+        z=int(drch);
+        drch=drch-z;
+        ee=to_string(z);
+        if(z==10){
+        ee='A';
+        }
+        if(z==11){
+        ee='B';
+        }
+        if(z==12){
+        ee='C';
+        }
+        if(z==13){
+        ee='D';
+        }
+        if(z==14){
+        ee='E';
+        }
+        if(z==15){
+        ee='F';
+        }
+        e=e+ee;
+        }
+        }
     }
-    e=m+e;
     return e;
 }
-int bbinary(int k[], int w, int s){
-    int z, y;
+double bbinary(int k[], int w, int s, int x){
+    double z, y;
+    int j=x+1;
     y=0;
-    for(int h=0; h<w; h++){
-        z=k[h]*pow(s, (w-h-1));
+    for(int h=0; h<x; h++){
+        z=k[h]*pow(s, (x-h-1));
         y+=z;
+    }
+    for(x=-1; j<w; x--){
+    z=k[j]*pow(s, x);
+    y+=z;
+    j++;
     }
     return y;
 }
-int ibinary(string n, int s){
-    int l, q;
+double ibinary(string n, int s){
+    int q;
+    double l;
     q=n.length();
+    int x=q;
     if(n[0]=='-'){
             q--;
+            x--;
     }
     char pp[q];
     if(n[0]!='-'){
@@ -69,7 +107,7 @@ int ibinary(string n, int s){
     }
     int ch[q];
     for(int k=0; k<q; k++){
-        if(pp[k]=='A' or pp[k]=='B' or pp[k]=='C' or pp[k]=='D' or pp[k]=='E' or pp[k]=='F' or pp[k]=='a' or pp[k]=='b' or pp[k]=='c' or pp[k]=='d' or pp[k]=='e' or pp[k]=='f'){
+        if(pp[k]=='A' or pp[k]=='B' or pp[k]=='C' or pp[k]=='D' or pp[k]=='E' or pp[k]=='F' or pp[k]=='a' or pp[k]=='b' or pp[k]=='c' or pp[k]=='d' or pp[k]=='e' or pp[k]=='f' or pp[k]==',' or pp[k]=='.'){
         if(pp[k]=='A' or pp[k]=='a'){
             ch[k]=10;
         }
@@ -88,12 +126,19 @@ int ibinary(string n, int s){
         if(pp[k]=='F' or pp[k]=='f'){
             ch[k]=15;
         }
+        if(pp[k]=='.' or pp[k]==','){
+        x=k;
+        if((q-1)!=k){
+            ch[k]=-1;
+        }
+        else{ch[k]=0;}
+        }
         }
         else{
         ch[k]=pp[k]-'0';
         }
     }
-    l=bbinary(ch, q, s);
+    l=bbinary(ch, q, s, x);
     if(n[0]=='-'){
         l*=(-1);
     }
