257288a4f2f16d275c422e64e810f1d7162f4ade
database-moscow <ispoap@gmail.com>
2025-03-26 22:39:24 +0700

    Add files via upload

---
+++ hexadecimal.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+int main(){
+    int n, m;
+    cin>>hex>>n;
+    cin>>dec>>m;
+    cout<<dec<<n<<" ";
+    cout<<hex<<m;
+    return 0;
+}
---
+++ octal.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+int main(){
+    int n, m;
+    cin>>oct>>n;
+    cin>>dec>>m;
+    cout<<dec<<n<<" ";
+    cout<<oct<<m;
+    return 0;
+}
